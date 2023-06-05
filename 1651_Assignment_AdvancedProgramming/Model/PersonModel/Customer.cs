2f971e5eb46f76960b88757fce3a6cacf57b216c
qngoc07012002 <116235732+qngoc07012002@users.noreply.github.com>
2023-06-05 11:57:25 +0700

    Update Customer.cs

--- 1651_Assignment_AdvancedProgramming/Model/PersonModel/Customer.cs
+++ 1651_Assignment_AdvancedProgramming/Model/PersonModel/Customer.cs
@@ -4,9 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _1651_Assignment_AdvancedProgramming.Model.Person
+namespace _1651_Assignment_AdvancedProgramming.Model.PersonModel
 {
-    internal class Customer
+    internal class Customer : Person
     {
+        private string address;
+
+        public string Address { get { return address; } set { address = value; } }
+
+        public override void displayInformation()
+        {
+            Console.WriteLine(base.Name," ",address);
+        }
     }
 }
