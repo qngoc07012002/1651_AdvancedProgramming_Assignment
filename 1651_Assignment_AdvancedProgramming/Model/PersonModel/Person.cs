7ab7db29bd4b0d74ab684d3c9d3ae56a006d0237
qngoc07012002 <116235732+qngoc07012002@users.noreply.github.com>
2023-06-05 11:57:28 +0700

    Update Person.cs

--- 1651_Assignment_AdvancedProgramming/Model/PersonModel/Person.cs
+++ 1651_Assignment_AdvancedProgramming/Model/PersonModel/Person.cs
@@ -4,9 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _1651_Assignment_AdvancedProgramming.Model.Person
+namespace _1651_Assignment_AdvancedProgramming.Model.PersonModel
 {
-    internal class Person
+    internal abstract class Person
     {
+        private int id;
+        private string name;
+        private string age;
+        private string phoneNumber;
+        
+        public int Id { get { return id; }  }
+        public string Name { get { return name; } set { name = value; } }
+        public string Age { get { return age; } set { age = value; } }
+        public string PhoneNumber { get { return phoneNumber; } set { phoneNumber = value; } }
+
+        public abstract void displayInformation();
     }
 }
