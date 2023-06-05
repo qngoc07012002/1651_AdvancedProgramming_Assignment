bee60941488f814c708cbf70f703b6e094e364ef
qngoc07012002 <116235732+qngoc07012002@users.noreply.github.com>
2023-06-05 11:57:17 +0700

    Update ProductController.cs

--- 1651_Assignment_AdvancedProgramming/Controller/ProductController.cs
+++ 1651_Assignment_AdvancedProgramming/Controller/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _1651_Assignment_AdvancedProgramming.Model.ProductModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,36 @@ namespace _1651_Assignment_AdvancedProgramming.Controller
 {
     internal class ProductController
     {
+        private List<Product> listProduct;
+
+        public void addProduct()
+        {
+
+        }
+
+        public void removeProduct()
+        {
+
+        }
+
+        public void editProduct()
+        {
+
+        }
+
+        public void displayAllProduct()
+        {
+
+        }
+
+        public Product getProductByID(int id)
+        {
+            return null;
+        }
+
+        public Product getProductByName(string name)
+        {
+            return null;
+        }
     }
 }
