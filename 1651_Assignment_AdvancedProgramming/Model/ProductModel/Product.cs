2796fce76a102a3cf343eaacdbd1c7d05319c4bf
qngoc07012002 <116235732+qngoc07012002@users.noreply.github.com>
2023-06-05 11:57:38 +0700

    Update Product.cs

--- 1651_Assignment_AdvancedProgramming/Model/ProductModel/Product.cs
+++ 1651_Assignment_AdvancedProgramming/Model/ProductModel/Product.cs
@@ -4,9 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _1651_Assignment_AdvancedProgramming.Model.Product
+namespace _1651_Assignment_AdvancedProgramming.Model.ProductModel
 {
-    internal class Product
+    internal abstract class Product
     {
+        private int id;
+        private string name;
+        private double price;
+        private int quantity;
+        private string category;
+
+        public int Id { get { return id; } set { id = value; } }    
+        public string Name { get { return name; } set { name = value; } }
+        public double Price { get { return price; } set { price = value; } }
+        public int Quantity { get { return quantity; } set { quantity = value; } }
+
+        public abstract string getCategory();
     }
 }
