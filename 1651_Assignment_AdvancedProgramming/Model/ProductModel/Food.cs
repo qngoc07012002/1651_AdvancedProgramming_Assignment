d1ee2c68b46e243bc18297121ceae0b3da37ddb0
qngoc07012002 <116235732+qngoc07012002@users.noreply.github.com>
2023-06-05 11:57:35 +0700

    Update Food.cs

--- 1651_Assignment_AdvancedProgramming/Model/ProductModel/Food.cs
+++ 1651_Assignment_AdvancedProgramming/Model/ProductModel/Food.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _1651_Assignment_AdvancedProgramming.Model.Product
+namespace _1651_Assignment_AdvancedProgramming.Model.ProductModel
 {
-    internal class Food
+    internal class Food : Product
     {
+        public override string getCategory()
+        {
+            return this.GetType().Name;
+        }
     }
 }
