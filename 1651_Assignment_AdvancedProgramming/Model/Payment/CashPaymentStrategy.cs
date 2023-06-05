fee5a15ce5cb4e098a714e584c8b597a11ca2d9b
qngoc07012002 <116235732+qngoc07012002@users.noreply.github.com>
2023-06-05 11:57:20 +0700

    Update CashPaymentStrategy.cs

--- 1651_Assignment_AdvancedProgramming/Model/Payment/CashPaymentStrategy.cs
+++ 1651_Assignment_AdvancedProgramming/Model/Payment/CashPaymentStrategy.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace _1651_Assignment_AdvancedProgramming.Model.Payment
 {
-    internal class CashPaymentStrategy
+    internal class CashPaymentStrategy : IPaymentStrategy
     {
+        private double cashTendered;
+
+        public void CashPayment()
+        {
+
+        }
+
+        public void ProcessPayment(double amount)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
