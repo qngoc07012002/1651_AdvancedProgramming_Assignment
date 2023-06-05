fdd86236f14aecb924397ea87c7d083b3e7abaf2
qngoc07012002 <116235732+qngoc07012002@users.noreply.github.com>
2023-06-05 09:42:03 +0700

    Create Employee.cs

---
+++ 1651_Assignment_AdvancedProgramming/Model/PersonModel/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1651_Assignment_AdvancedProgramming.Model.Person
+{
+    internal class Employee : Person
+    {
+        private string position;
+
+        public string Position { get { return position; } set { position = value; } }
+
+        public override void displayInformation()
+        {
+            Console.WriteLine(base.Name, " ", position);
+        }
+    }
+}
