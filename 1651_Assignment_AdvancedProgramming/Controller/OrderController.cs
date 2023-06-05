f34aeefb574f1325e8e237ea96b8bddc9e0725b6
qngoc07012002 <116235732+qngoc07012002@users.noreply.github.com>
2023-06-05 09:41:00 +0700

    Create OrderController.cs

---
+++ 1651_Assignment_AdvancedProgramming/Controller/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using _1651_Assignment_AdvancedProgramming.Model.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1651_Assignment_AdvancedProgramming.Controller
+{
+    internal class OrderController
+    {
+        private List<Order> listOrder;
+
+        public void addOrder()
+        {
+
+        }
+
+        public void displayAllOrder()
+        {
+
+        }
+
+        public void displayAllOrderByCustomer()
+        {
+
+        }
+    }
+}
