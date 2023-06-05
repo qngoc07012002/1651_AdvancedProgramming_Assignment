e649484e0c65a34521e135b38e0b46a00670e746
qngoc07012002 <116235732+qngoc07012002@users.noreply.github.com>
2023-06-05 09:41:54 +0700

    Create CreditCardPaymentStrategy.cs

---
+++ 1651_Assignment_AdvancedProgramming/Model/Payment/CreditCardPaymentStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1651_Assignment_AdvancedProgramming.Model.Payment
+{
+    internal class CreditCardPaymentStrategy
+    {
+    }
+}
