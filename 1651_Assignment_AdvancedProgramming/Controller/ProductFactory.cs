2de2da69152fdd1d1deb5c85b10ce17c013f8971
qngoc07012002 <116235732+qngoc07012002@users.noreply.github.com>
2023-06-05 11:57:19 +0700

    Update ProductFactory.cs

--- 1651_Assignment_AdvancedProgramming/Controller/ProductFactory.cs
+++ 1651_Assignment_AdvancedProgramming/Controller/ProductFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _1651_Assignment_AdvancedProgramming.Model.ProductModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,19 @@ namespace _1651_Assignment_AdvancedProgramming.Controller
 {
     internal class ProductFactory
     {
+        public static Product getProduct(ProductCategory productCategory)
+        {
+            switch (productCategory)
+            {
+                case ProductCategory.Food:
+                    return new Food();
+                case ProductCategory.Drink:
+                    return new Drink();
+                case ProductCategory.PersonalItem:
+                    return new PersonalItem();
+                default:
+                    return null;
+            }
+        }
     }
 }
