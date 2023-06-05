d680913f96cfe5cd050f027d08577c723c61d646
qngoc07012002 <116235732+qngoc07012002@users.noreply.github.com>
2023-06-05 08:09:40 +0700

    Create Project

---
+++ 1651_Assignment_AdvancedProgramming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1651_Assignment_AdvancedProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
