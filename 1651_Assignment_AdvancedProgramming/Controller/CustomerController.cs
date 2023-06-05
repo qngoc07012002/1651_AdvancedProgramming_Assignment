7372e027d7cc3914e889fa5d782ea0c7abec9695
qngoc07012002 <116235732+qngoc07012002@users.noreply.github.com>
2023-06-05 11:57:14 +0700

    Update CustomerController.cs

--- 1651_Assignment_AdvancedProgramming/Controller/CustomerController.cs
+++ 1651_Assignment_AdvancedProgramming/Controller/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _1651_Assignment_AdvancedProgramming.Model.PersonModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,31 @@ namespace _1651_Assignment_AdvancedProgramming.Controller
 {
     internal class CustomerController
     {
+        private List<Customer> listCustomer;
+
+        public void addCustomer()
+        {
+
+        }
+
+        public void removeCustomer()
+        {
+
+        }
+
+        public void editCustomer()
+        {
+
+        }
+
+        public void displayAllCustomer()
+        {
+
+        }
+
+        public Customer getCustomerByID(int id)
+        {
+            return null
+        }
     }
 }
