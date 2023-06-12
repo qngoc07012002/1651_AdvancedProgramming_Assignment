8ae65ec8e212ad5c6bbbbaf449ff252b82f65bc9
qngoc07012002 <116235732+qngoc07012002@users.noreply.github.com>
2023-06-12 08:44:08 +0700

    Update Customer.cs

--- 1651_Assignment_AdvancedProgramming/Model/PersonModel/Customer.cs
+++ 1651_Assignment_AdvancedProgramming/Model/PersonModel/Customer.cs
@@ -13,6 +13,18 @@ namespace _1651_Assignment_AdvancedProgramming.Model.PersonModel
 
         public string Address { get { return address; } set { address = value; } }
 
+        public override void enterInformation()
+        {
+            Console.Write("Enter Name: ");
+            base.Name = Console.ReadLine();
+            Console.Write("Enter Age: ");
+            base.Age = int.Parse(Console.ReadLine());
+            Console.Write("Enter Phone Number: ");
+            base.PhoneNumber = Console.ReadLine();
+            Console.Write("Enter Address: ");
+            Address = Console.ReadLine();
+        }
+
         public override void displayInformation()
         {
             Console.WriteLine(base.Name," ",address);
