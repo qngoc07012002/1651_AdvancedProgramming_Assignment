9afc492fddd0ed14131561622553740c4a38f2b2
qngoc07012002 <116235732+qngoc07012002@users.noreply.github.com>
2023-06-12 07:09:25 +0700

    Update Customer.cs

--- 1651_Assignment_AdvancedProgramming/Model/PersonModel/Customer.cs
+++ 1651_Assignment_AdvancedProgramming/Model/PersonModel/Customer.cs
@@ -20,43 +20,6 @@ namespace _1651_Assignment_AdvancedProgramming.Model.PersonModel
 
         
   
-        SQLiteConnection conn = new SQLiteConnection("Data Source=StoreManagement.db");
-        private void SelectData(SQLiteConnection conn)
-        {
-            var id = "ID";
-            var fullName = "Name";
-            var age = "Age";
-            var phoneNumber = "PhoneNumber";
-            var address = "Address";
-            Console.WriteLine($"{id,-20:s}{fullName,-35:s}{age,-20:d}{phoneNumber,10:d}{address,20:d}");
-            var sql = "SELECT * FROM Customer";
-            var cmd = new SQLiteCommand(sql, conn);
-            var reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-                Console.WriteLine($"{reader.GetInt32(0),-20:d}" +
-                    $"{reader.GetString(1),-35:s}" +
-                    $"{reader.GetInt32(2),-20:d}" +
-                     $"{reader.GetString(3),10:s}" +
-                    $"{reader.GetString(4),20:s}");
-            }
-        }
 
-        public void displayCustomer()
-        {
-            try
-            {
-                conn.Open();
-                SelectData(conn);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-            finally
-            {
-                conn.Close();
-            }
-        }
     }
 }
